cb530720f5c16003ea750ba6080df389e40d9eca
Cavaldos <khanhnguyen297kn@gmail.com>
2024-07-06 18:56:04 +0700

    sql

--- servers/src/routes/product.route.ts
+++
@@ -1,9 +0,0 @@
-import ProductController from "../controllers/Product.co";
-import { Router } from "express";
-const router = Router();
-router.post("/aa", ProductController.createProduct);
-router.get("/bb", ProductController.getProducts);
-router.get("/:id", ProductController.getProduct);
-router.put("/:id", ProductController.updateProduct);
-router.delete("/:id", ProductController.deleteProduct);
-export default router;
